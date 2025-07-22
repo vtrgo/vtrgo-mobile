c1436b30f1c995af39b4823f520f64a044099a7e
Jon-Hincks <hincksj5@gmail.com>
2025-07-22 10:43:42 -0400

    Improved tootip

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {useDerivedValue, type SharedValue} from "react-native-reanimated"
 NfcManager.start();
 
 function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
-  return <Circle cx={x} cy={y} r={8} color="black" />;
+  return <Circle cx={x} cy={y} r={8} color="grey" opacity={0.8} />;
 }
 
 function printStructuredJson(data: Record<string, any>) {
